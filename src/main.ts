8528a10d2ad14f7694ec333f9892dc6fe69eb8e0
oriol <ogomez.s92@gmail.com>
2023-03-01 19:05:03 +0100

    feat: setup navigator language in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import './style.css'
 import App from './App.vue'
 import TranslationsManager from '@src/managers/translations_manager'
 
-const translationsManager = new TranslationsManager('es');
+const translationsManager = new TranslationsManager(navigator.language);
 
 createApp(App)
 .use(translationsManager.getVuePlugin())
