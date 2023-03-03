98269b09b76e09e94bc0e3e15a1d1a6adb029eb5
oriol <ogomez.s92@gmail.com>
2023-03-03 07:15:18 +0100

    fix: fix broken navigator language because it was getting regional language too

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import './style.css'
 import App from './App.vue'
 import TranslationsManager from '@src/managers/translations_manager'
 
-const translationsManager = new TranslationsManager(navigator.language);
+const translationsManager = new TranslationsManager(navigator.language.split('-')[0]);
 
 createApp(App)
 .use(translationsManager.getVuePlugin())
