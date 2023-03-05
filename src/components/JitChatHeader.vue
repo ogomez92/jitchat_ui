f1d8412103541e94e58fe98087ffa21de464f717
oriol <ogomez.s92@gmail.com>
2023-03-05 11:22:45 +0100

    feat: add header css and logo

--- src/components/JitChatHeader.vue
+++ src/components/JitChatHeader.vue
@@ -1,3 +1,10 @@
 <template>
-  <h1 class="text-xl">JitChat</h1>
+  <header class="flex items-center justify-center bg-white py-4 shadow-md">
+    <img :src="JitchatLogo" alt="JitChat Logo" class="h-8 w-8 mr-2" />
+    <h1 class="text-xl font-bold">JitChat</h1>
+  </header>
 </template>
+
+<script setup lang="ts">
+  import JitchatLogo from '@src/assets/logo.png';
+  </script>
